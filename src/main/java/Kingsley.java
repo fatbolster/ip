e5e8d23c1dedf13bbaea802ee1cdc30b5ff65a2b
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2025-08-21 16:58:35 +0800

    Added task storing functionality

--- src/main/java/Kingsley.java
+++ src/main/java/Kingsley.java
@@ -3,26 +3,41 @@ import java.util.Scanner;
 public class Kingsley {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         System.out.println("    ------------------------------------------");
         System.out.println("    Hello! I'm Kingsley");
         System.out.println("    What can I do for you?");
-        System.out.println("    ------------------------------------------");
+        System.out.println("    __________________________________________");
 
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println("    ------------------------------------------");
-            System.out.println("    " + input);
-            System.out.println("    ------------------------------------------");
+            if (input.equals("list")) {
+                System.out.println("    ___________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    int taskNumber = i + 1;
+                    System.out.println("    " + taskNumber + ". " + tasks[i]);
+                }
+                System.out.println("    ___________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("    ___________________________________________");
+                System.out.println("     added: " + input);
+                System.out.println("    ___________________________________________");
+
+            }
+
 
 
         }
 
 
-        System.out.println("    ------------------------------------------");
+        System.out.println("    ___________________________________________");
         System.out.println("    Bye! Hope to see you again soon!");
-        System.out.println("    ------------------------------------------");
+        System.out.println("    ___________________________________________");
     }
 }
