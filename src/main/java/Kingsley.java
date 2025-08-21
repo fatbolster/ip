2f53e4f3e8c3d33fc2007783a2e0072cbc62d2f2
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2025-08-21 16:40:12 +0800

    Added input echoing feature

--- src/main/java/Kingsley.java
+++ src/main/java/Kingsley.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Kingsley {
     public static void main(String[] args) {
-        System.out.println("__________________________________________");
-        System.out.println("Hello! I'm Kingsley");
-        System.out.println("What can I do for you?");
-        System.out.println("___________________________________________");
-        System.out.println("Bye! Hope to see you again soon!");
-        System.out.println("___________________________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    ------------------------------------------");
+        System.out.println("    Hello! I'm Kingsley");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ------------------------------------------");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("    ------------------------------------------");
+            System.out.println("    " + input);
+            System.out.println("    ------------------------------------------");
+
+
+        }
+
+
+        System.out.println("    ------------------------------------------");
+        System.out.println("    Bye! Hope to see you again soon!");
+        System.out.println("    ------------------------------------------");
     }
 }
