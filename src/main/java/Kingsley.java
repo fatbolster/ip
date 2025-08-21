6f1f0fd3300c8a3eca6fca997f6c18297a024932
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2025-08-21 16:29:15 +0800

    Added a greeting message

---
+++ src/main/java/Kingsley.java
@@ -0,0 +1,10 @@
+public class Kingsley {
+    public static void main(String[] args) {
+        System.out.println("__________________________________________");
+        System.out.println("Hello! I'm Kingsley");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________________");
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("___________________________________________");
+    }
+}
