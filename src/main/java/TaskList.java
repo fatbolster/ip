4ceecd9f128345adb818cd1ecadc4e10f5177a1f
Chua Kai Jie, Emerson <emersonchua934@outlook.com>
2025-08-31 17:31:24 +0800

    Add TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public Task remove(int taskNumber) {
+        return tasks.remove(taskNumber);
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public Task get(int taskNumber) {
+        return tasks.get(taskNumber);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public ArrayList<Task> getTaskList() {
+        return this.tasks;
+    }
+
+
+
+
+
+
 }
